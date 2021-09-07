f561e887ef4c9ca13b12acd42cd4f8dc0e4e27dd
joaquinabal <joabal97@gmail.com>
2021-09-07 01:01:38 -0300

    ProductCart.html + styles.css correspondientes + imagen PS5 Prueba

--- app.js
+++ app.js
@@ -12,3 +12,7 @@ app.listen(3000, () => {
 app.get ('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 })
+
+app.get ('/productCart', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+})
