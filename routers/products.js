36543a8c0f63c3eb4913cdc0f0288a2d44257fbd
Manuel98017 <88805396+Manuel98017@users.noreply.github.com>
2021-11-18 22:28:46 -0300

    Arreglo una pavada

--- routers/products.js
+++ routers/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const multer = require('multer');
-const userLogged = require('../middlewares/userAuth.js');
+const userLogged = require('../middlewares/userLogged.js');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
