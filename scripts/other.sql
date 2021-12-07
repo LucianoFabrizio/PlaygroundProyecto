3448173a80714436cb970238a5b0aad54f3ddf24
Manuel <nanoprozzi@gmail.com>
2021-12-06 22:04:23 -0300

    Creo scriipts para completar la base, no anda el de productos porque creé mal las FK

---
+++ scripts/other.sql
@@ -0,0 +1,20 @@
+-- Brands
+
+insert into brands (name) values ('Sony');
+insert into brands (name) values ('Nintendo');
+insert into brands (name) values ('Microsoft');
+
+-- Categories
+
+insert into categories (name) values ('console');
+insert into categories (name) values ('game');
+insert into categories (name) values ('accessory');
+
+-- States
+
+insert into states (name) values ('placed');
+insert into states (name) values ('checked');
+insert into states (name) values ('sent');
+insert into states (name) values ('recieved');
+insert into states (name) values ('archived');
+
