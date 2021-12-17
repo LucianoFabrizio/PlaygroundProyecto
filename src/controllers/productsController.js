d13a5bd145cb7a1e31017aeeb9a337569d5cd608
Manuel <nanoprozzi@gmail.com>
2021-12-16 22:30:41 -0300

    Avanzando lentamente con la edición de produ

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,42 +1,37 @@
 const db = require('../database/models');
-const { Op } = require("sequelize")
-
+const { Op } = require('sequelize');
 
 const controller = {
-    list: function(req, res) {
-        db.Product.findAll()
-            .then(function(product) {
-                console.log(product);
-                
-            })
+    list: function (req, res) {
+        db.Product.findAll().then(function (product) {
+            console.log(product);
+        });
     },
 
-    search: function(req,res) {
-        const prodName = req.query.prodSearch
-        
+    search: function (req, res) {
+        const prodName = req.query.prodSearch;
+
         db.Product.findAll({
             where: {
                 name: {
-                    [Op.like]:
-                 '%' + prodName + '%'
-                }
-            }
+                    [Op.like]: '%' + prodName + '%',
+                },
+            },
         })
-        .then((product) => {
-            console.log(product)
-            res.render('products.ejs', { product })
+            .then((product) => {
+                console.log(product);
+                res.render('products.ejs', { product });
 
-            // res.redirect('/products/')
-        })
-        .catch(error => res.redirect('/'))
+                // res.redirect('/products/')
+            })
+            .catch((error) => res.redirect('/'));
     },
 
-
-    create: function(req, res) {
-        res.render('product-create.ejs')
+    create: function (req, res) {
+        res.render('product-create.ejs');
     },
 
-    processCreate: function(req, res) {
+    processCreate: function (req, res) {
         console.log(req.body);
         db.Product.create({
             name: req.body.name,
@@ -47,7 +42,7 @@ const controller = {
             price: req.body.price,
             gallery: null,
             category_id: req.body.category,
-            brand_id: req.body.brand            
+            brand_id: req.body.brand,
         })
             .then(() => {
                 return res.redirect('/');
@@ -55,55 +50,55 @@ const controller = {
             .catch((error) => res.send(error));
     },
 
-    edit: function(req, res) {
-     res.render('product-edit.ejs')
+    edit: function (req, res) {
+        db.Product.findByPk(req.params.id)
+            .then((product) => {
+                res.render('product-edit.ejs', { product });
+            })
+            .catch((error) => res.send(error));
     },
 
-    processEdit: function(req, res) {
+    processEdit: function (req, res) {
         let prodId = req.params.id;
-        Users
-        .update(
+        db.Product.update(
             {
-                    name: req.body.name,
-                    image: req.body.image,
-                    creation_date: Date.now(),
-                    modified_date: Date.now(),
-                    deletion_date: null,
-                    price: req.body.precio,
-                    gallery: req.body.imgProd,
-                    category_id: req.body.tipoProd,
-                    brand_id: req.body.marca
-        
-                
+                name: req.body.name,
+                image: req.body.image,
+                creation_date: Date.now(),
+                modified_date: Date.now(),
+                deletion_date: null,
+                price: req.body.precio,
+                gallery: req.body.imgProd,
+                category_id: req.body.tipoProd,
+                brand_id: req.body.marca,
             },
             {
-                where: {id: prodId}
+                where: { id: prodId },
+            }
+        )
+            .then(() => {
+                return res.redirect('/');
             })
-        .then(()=> {
-            return res.redirect('/')})
-        .catch(error => res.send(error))
+            .catch((error) => res.send(error));
     },
-     delete: function (req,res) {
-    },
+    delete: function (req, res) {},
 
-    detail: function(req, res) {
+    detail: function (req, res) {
         db.Product.findByPk(req.params.id)
             .then((product) => {
                 res.render('detail.ejs', { product });
             })
             .catch((error) => res.send(error));
     },
-    
 
-    delete: function(req, res) {
+    delete: function (req, res) {
         let productId = req.params.id;
-        Users
-        .destroy({where: {id: productId}, force: true}) // force: true es para asegurar que se ejecute la acción
-        .then(()=>{
-            return res.redirect('/')})
-        .catch(error => res.send(error))
-    }
-}
-
+        Users.destroy({ where: { id: productId }, force: true }) // force: true es para asegurar que se ejecute la acción
+            .then(() => {
+                return res.redirect('/');
+            })
+            .catch((error) => res.send(error));
+    },
+};
 
 module.exports = controller;
