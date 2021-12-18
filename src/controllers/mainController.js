2df726d3be152426125fcb13b46f74ec46a8b4fd
Manuel <nanoprozzi@gmail.com>
2021-12-18 15:54:44 -0300

    Arreglo index

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,16 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const db = require('../database/models');
+const { Op } = require('sequelize');
+
+const Products = db.Product;
+
+
+            
+
+
 
 const controlador = {
     index: function (req, res) {
-        Products.findAll()
+        Products.findAll({
+            raw: true,
+           })
             .then((products) => {
-                products.forEach(element => {
-                    
+                const consoles = products.filter(function (product) {
+                    return product.category_id == 1;
                 });
-                res.render('index.ejs', { products });
+                const games = products.filter(function (product) {
+                    return product.category_id == 2;
+                });
+                const accessories = products.filter(function (product) {
+                    return product.category_id == 3;
+                });
+                console.log(consoles, games);
+                res.render('index.ejs', { consoles, games, accessories });
             })
-            .catch((error) => res.redirect('/'));
+            .catch((error) => console.log(error));
     }
 };
 
