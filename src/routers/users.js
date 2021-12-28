01ae6566d2819365d1d3bd4d40c08d84179add67
joaquinabal <joabal97@gmail.com>
2021-12-27 22:39:55 -0300

    validacion back end de usuario, register y login HECHO

--- src/routers/users.js
+++ src/routers/users.js
@@ -3,38 +3,66 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 const { body } = require('express-validator');
 
-const validations = [
-    body('name').notEmpty().withMessage('Tienes que escribir un Nombre'),
-    body('mail').notEmpty().withMessage('Tienes que escribir un mail'),
+const validationsRegistro = [
+    body('name')
+    .notEmpty().
+    withMessage('Tienes que escribir un Nombre'),
+
+    body('name')
+    .isLength({ min: 2 })
+    .withMessage('El usuario debe tener al menos 2 caracteres'),
+
+
+    body('mail')
+    .notEmpty()
+    .withMessage('Tienes que escribir un mail'),
+
     body('password')
-        .notEmpty()
-        .withMessage('Tienes que escribir una contraseña'),
-    // .matches('password2').withMessage('Tienes que escribir la misma contraseña en los dos campos'),
+    .notEmpty()
+    .withMessage('Tienes que escribir una contraseña'),
+    
+    body('password')
+   .isLength({ min: 8 })
+   .withMessage('La contraseña debe tener al menos 8 caracteres'),
+       // .matches('password2').withMessage('Tienes que escribir la misma contraseña en los dos campos'),
     body('password2')
         .notEmpty()
-        .withMessage('Tienes que escribir una contraseña')
+        .withMessage('Tienes que escribir una contraseña'),
+        body('password2')
+        .isLength({ min: 8 })
+       .withMessage('La contraseña debe tener al menos 8 caracteres')
         .custom((value, { req }) => {
             if (value !== req.body.password) {
                 throw new Error(
-                    'Password confirmation does not match password'
+                    'las contraseñas no son iguales'
                 );
             }
             return true;
         }),
+
 ];
+
+const validationsLogin = [
+    body('email')
+    .notEmpty(),
+
+    body('password')
+    .notEmpty()
+    .withMessage('Tienes que escribir una contraseña'),
+]
 
 
 router.get('/list', usersController.list);
 
 router.get('/detail/:id', usersController.detail);
 
-router.get('/register', validations, usersController.register);
+router.get('/register', usersController.register);
 
-router.post('/create', usersController.create);
+router.post('/create', validationsRegistro, usersController.create);
 
 router.get('/login', usersController.login)
 
-router.post('/login', usersController.processLogin)
+router.post('/login', validationsLogin, usersController.processLogin)
 
 router.get('/edit/:id', usersController.edit);
 
