740659d7b43f6415d6a3af46a157e8ce0d8f90ac
joaquinabal <joabal97@gmail.com>
2022-01-14 12:23:55 -0300

    countbycategory en funcionamiento

--- src/controllers/api/apiProductsController.js
+++ src/controllers/api/apiProductsController.js
@@ -7,6 +7,11 @@ const controller = {
         
         let urlProduct = "http://localhost:3000/products/detail/"
         let arrayProducts = [];
+        let objectCategories = {
+            consola: 0,
+            juego: 0,
+            accesorio: 0
+        }
     Products.findAll({
         raw: true
     })
@@ -22,10 +27,23 @@ const controller = {
                 URL: urlProduct + product.id
             }
         ))
+            console.log(e.length)
+        for (let i = 0; i < e.length ; i++){
+     
 
+            if (e[i].category_id == 1){
+                objectCategories.consola += 1
+            } else if (e[i].category_id == 2){
+                objectCategories.juego += 1
+            } else if (e[i].category_id == 3){
+                objectCategories.accesorio += 1
+        }
+    }
             let dataProducts = {
                 count: e.length,
+                countByCategory: objectCategories,
                 products: arrayProducts
+
             }
             return res.json(dataProducts)
 
