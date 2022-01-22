3ce81e3301118c05cf7e55187910510849050253
Manuel <nanoprozzi@gmail.com>
2022-01-22 15:02:45 -0300

    Arreglo banner

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -21,6 +21,69 @@ const controlador = {
                 res.render('index.ejs', { consoles, games, accessories });
             })
             .catch((error) => console.log(error));
+    },
+    xbox: function (req, res) {
+        Products.findAll({
+            where: {
+                brand_id: 3
+            },
+            raw: true,
+          })
+            .then((products) => {
+                const consoles = products.filter(function (product) {
+                    return product.category_id == 1;
+                });
+                const games = products.filter(function (product) {
+                    return product.category_id == 2;
+                });
+                const accessories = products.filter(function (product) {
+                    return product.category_id == 3;
+                });
+                res.render('index.ejs', { consoles, games, accessories });
+            })
+            .catch((error) => console.log(error));
+    },
+    playstation: function (req, res) {
+        Products.findAll({
+            where: {
+                brand_id: 1
+            },
+            raw: true,
+          })
+            .then((products) => {
+                const consoles = products.filter(function (product) {
+                    return product.category_id == 1;
+                });
+                const games = products.filter(function (product) {
+                    return product.category_id == 2;
+                });
+                const accessories = products.filter(function (product) {
+                    return product.category_id == 3;
+                });
+                res.render('index.ejs', { consoles, games, accessories });
+            })
+            .catch((error) => console.log(error));
+    },
+    switch: function (req, res) {
+        Products.findAll({
+            where: {
+                brand_id: 2
+            },
+            raw: true,
+          })
+            .then((products) => {
+                const consoles = products.filter(function (product) {
+                    return product.category_id == 1;
+                });
+                const games = products.filter(function (product) {
+                    return product.category_id == 2;
+                });
+                const accessories = products.filter(function (product) {
+                    return product.category_id == 3;
+                });
+                res.render('index.ejs', { consoles, games, accessories });
+            })
+            .catch((error) => console.log(error));
     }
 };
 
--- src/routers/main.js
+++ src/routers/main.js
@@ -6,4 +6,13 @@ const mainController = require('../controllers/mainController.js');
 // Index
 router.get('/', mainController.index);
 
+// Xbox
+router.get('/xbox', mainController.xbox);
+
+// PlayStation
+router.get('/playstation', mainController.playstation);
+
+// Switch
+router.get('/switch', mainController.switch);
+
 module.exports = router;
