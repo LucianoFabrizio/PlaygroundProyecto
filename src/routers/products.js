675b6afcd4cbc05c75357af0a012caaedc30891c
Manuel <nanoprozzi@gmail.com>
2022-01-25 19:35:19 -0300

    Arreglo parcialmente edición y creación de productos

--- src/routers/products.js
+++ src/routers/products.js
@@ -3,79 +3,83 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 const path = require('path');
 const userLogged = require('../middlewares/userLogged.js');
-const multer  = require('multer')
+const multer = require('multer');
 const { body } = require('express-validator');
-const userAuth = require('../middlewares/userAuth')
+const userAuth = require('../middlewares/userAuth');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/uploads/products')
+        cb(null, 'public/uploads/products');
     },
     filename: function (req, file, cb) {
         let fileName = `${Date.now()}${path.extname(file.originalname)}`;
-		cb(null, fileName);
-    }
-  })
-  
-  const upload = multer({ storage: storage });
+        cb(null, fileName);
+    },
+});
+
+const upload = multer({ storage: storage });
 
 const validationProd = [
     body('name')
-    .notEmpty()
-    .withMessage('Tienes que escribir un nombre con al menos 5 caracteres.')
-    .isLength({ min: 5 })
-    .withMessage('Tienes que escribir un nombre con al menos 5 caracteres.'),
+        .notEmpty()
+        .withMessage('Tienes que escribir un nombre.')
+        .isLength({ min: 5, max: undefined })
+        .withMessage(
+            'Tienes que escribir un nombre con al menos 5 caracteres.'
+        ),
 
     body('description')
-    .notEmpty()
-    .withMessage('Tienes que escribir una descripción con al menos 20 caracteres.')
-    .isLength({ min: 20 })
-    .withMessage('Tienes que escribir una descripción con al menos 20 caracteres.'),
+        .notEmpty()
+        .withMessage('Tienes que escribir una descripción.')
+        .isLength({ min: 20, max: undefined })
+        .withMessage(
+            'Tienes que escribir una descripción con al menos 20 caracteres.'
+        ),
 
-    body('image')
-    .notEmpty()
-    .withMessage('Tienes que subir una imagen.')
-    .custom( function(value, filename) {
+    body('images').custom((value, { req }) => {
+        if (req.file != undefined) {
+            let file = req.file;
+            let extensionsAllowed = ['.jpeg', '.jpg', '.png', '.gif'];
+            let fileExtension = path.extname(file.originalname);
+            if (!extensionsAllowed.includes(fileExtension)) {
+                throw new Error(
+                    'Solo puedes usar archivos ' + extensionsAllowed.join(', ')
+                );
+            }
+        }
+        return true;
+    }),
+];
 
-      let extension = (path.extname(filename)).toLowerCase();
-      switch (extension) {
-          case '.jpg':
-              return '.jpg';
-          case '.jpeg':
-              return '.jpeg';
-          case  '.png':
-              return '.png';
-          case '.gif':
-          default:
-            throw new Error ('La extensión del archivo es incorrecta.')
-            
-      }
-  }
-)
-]
-
-
-
-
-// LISTAR 
+// LISTAR
 router.get('/', productsController.list);
 
-// ADMIN 
+// ADMIN
 router.get('/admin', userAuth, productsController.admin);
 
 // CREAR
-router.get ('/create', userAuth, productsController.create);
-router.post('/create',  validationProd, upload.single('image'), productsController.processCreate);
+router.get('/create', userAuth, productsController.create);
+router.post(
+    '/create',
+    upload.single('image'),
+    validationProd,
+    productsController.processCreate
+);
 
 // EDITAR
-router.get ('/edit/:id',validationProd, userAuth,  productsController.edit);
-router.put('/edit/:id',  validationProd,  upload.single('image'), productsController.processEdit);
+router.get('/edit/:id', validationProd, userAuth, productsController.edit);
+router.put(
+    '/edit/:id',
+    upload.single('image'),
+    validationProd,
+    productsController.processEdit
+);
 
 //DETALLE
 router.get('/detail/:id', productsController.detail);
 
 //BUSCAR
-router.get('/search', productsController.search)
+router.get('/search', productsController.search);
 
 // ELIMINAR
 router.delete('/delete/:id', productsController.delete);
